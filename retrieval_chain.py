bdd684d8049435aee869efa3d92eae93d31ed3f5
Amanda Waite <amandarwaite@gmail.com>
2024-03-03 20:39:08 +0000

    lesson 4 complete

--- retrieval_chain.py
+++ retrieval_chain.py
@@ -1,10 +1,13 @@
-from langchain_openai import ChatOpenAI
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 import os
 from dotenv import load_dotenv
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_community.document_loaders import WebBaseLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from bs4 import BeautifulSoup
+from langchain_community.vectorstores.faiss import FAISS
+from langchain.chains import create_retrieval_chain
 
 load_dotenv()
 
@@ -13,35 +16,50 @@ api_key = os.getenv("OPENAI_API_KEY")
 def get_documents_from_web(url):
     loader = WebBaseLoader(url)
     documents = loader.load()
-    return documents
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=200,
+        chunk_overlap=20
+    )
+    splitDocs = splitter.split_documents(documents)
 
-docs = get_documents_from_web("https://en.wikipedia.org/wiki/LCEL")
+    return splitDocs
 
-model = ChatOpenAI(
-    api_key=api_key,
-    model="gpt-3.5-turbo",
-    temperature=0.4
-)
+def create_vector_store(docs):
+    embedding = OpenAIEmbeddings()
+    vectoreStore = FAISS.from_documents(docs, embedding=embedding)
+    return vectoreStore
 
-prompt = ChatPromptTemplate.from_template("""
-    Answer the user's question:
-    Context: {context}    
-    Question: {input}
-""")
+def create_chain(vectorStore):
+    model = ChatOpenAI(
+        api_key=os.getenv("OPENAI_API_KEY"),
+        model="gpt-3.5-turbo",
+        temperature=0.4
+    )
 
-chain = create_stuff_documents_chain(
-    llm=model,
-    prompt=prompt
-)
+    prompt = ChatPromptTemplate.from_template("""
+        Answer the user's question:
+        Context: {context}    
+        Question: {input}
+    """)
 
-# chain = prompt | model
-print(type(chain))
+    chain = create_stuff_documents_chain(
+        llm=model,
+        prompt=prompt
+    )
+
+    retriever = vectorStore.as_retriever()
+    retrieval_chain = create_retrieval_chain(retriever, chain)
+
+    return retrieval_chain
+
+docs = get_documents_from_web("https://python.langchain.com/docs/expression_language/")
+vectorStore = create_vector_store(docs)
+
+chain = create_chain(vectorStore)
 
 response = chain.invoke({
     "input": "What is LCEL?",
-    "context": [docs]
 })
 
-print(response)
-
+print(response["answer"])
 
