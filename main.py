30557a170898f338e2ef9af74142c26beb6dfa1e
Amanda Waite <amandarwaite@gmail.com>
2024-03-03 13:54:00 +0000

    merged an example of using history

--- main.py
+++ main.py
@@ -1,32 +1,35 @@
 
 import gradio as gr
 from ollama import Client
+from langchain.schema import AIMessage, HumanMessage
 
 client = Client(host='http://localhost:11434')
 
 conversation_history = []
 
 
-def chat(prompt):
-    conversation_history.append(prompt)
-    prompt_with_history = '\n'.join(conversation_history)
+def chat(prompt, history):
 
-    response = client.chat(model='mario:latest', 
+    history_langchain_format = []
+
+    for human, ai in history:
+        history_langchain_format.append(HumanMessage(content=human))
+        history_langchain_format.append(AIMessage(content=ai))
+
+    history_langchain_format.append(HumanMessage(content=prompt))
+
+    response = client.chat(model='mistral:latest', 
                            messages=[
                             {'role': 'user', 
                              'keep_alive': '-1',
     
-                             'content': prompt_with_history
+                             'content': history_langchain_format
                              }])
     answer = response['message']['content']
 
-    conversation_history.append(answer)
-
-    print(conversation_history)
+    # conversation_history.append(answer)
+    # print(conversation_history)
 
     return answer
 
-
-iface = gr.Interface(fn=chat, inputs=gr.Textbox(
-    lines=2, placeholder="Enter your prompt here..."), outputs="text")
-iface.launch()
+iface = gr.ChatInterface(chat).launch()
