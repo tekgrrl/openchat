8004f07711f92ca5bbcf1d9d60958251e10044e6
Amanda Waite <amandarwaite@gmail.com>
2024-03-04 11:11:23 +0000

    Lesson 5

--- retrieval_chain.py
+++ retrieval_chain.py
@@ -1,23 +1,23 @@
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 import os
 from dotenv import load_dotenv
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_community.document_loaders import WebBaseLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from bs4 import BeautifulSoup
 from langchain_community.vectorstores.faiss import FAISS
 from langchain.chains import create_retrieval_chain
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain.chains.history_aware_retriever import create_history_aware_retriever
 
 load_dotenv()
-
-api_key = os.getenv("OPENAI_API_KEY")
 
 def get_documents_from_web(url):
     loader = WebBaseLoader(url)
     documents = loader.load()
     splitter = RecursiveCharacterTextSplitter(
-        chunk_size=200,
+        chunk_size=400,
         chunk_overlap=20
     )
     splitDocs = splitter.split_documents(documents)
@@ -31,35 +31,60 @@ def create_vector_store(docs):
 
 def create_chain(vectorStore):
     model = ChatOpenAI(
-        api_key=os.getenv("OPENAI_API_KEY"),
         model="gpt-3.5-turbo",
         temperature=0.4
     )
 
-    prompt = ChatPromptTemplate.from_template("""
-        Answer the user's question:
-        Context: {context}    
-        Question: {input}
-    """)
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", "Anser the user's question based on the context: {context}"),
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("human", "{input}")
+    ])
 
     chain = create_stuff_documents_chain(
         llm=model,
         prompt=prompt
     )
 
-    retriever = vectorStore.as_retriever()
-    retrieval_chain = create_retrieval_chain(retriever, chain)
+    retriever = vectorStore.as_retriever(searhc_kwargs={"k": 3})
+    retriever_prompt = ChatPromptTemplate.from_messages([
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("human", "{input}"),
+        ("human", "Given the above conversation, generate a search query to look up in order to get information relevant to the conversation.")
+    ])
+
+    # Wraps the original retriever with a history-aware retriever
+    history_aware_retriever = create_history_aware_retriever(
+        llm=model,
+        retriever=retriever,
+        prompt=retriever_prompt
+    )
+    retrieval_chain = create_retrieval_chain(history_aware_retriever, chain) # Was using the unwrapped retriever here
 
     return retrieval_chain
 
-docs = get_documents_from_web("https://python.langchain.com/docs/expression_language/")
-vectorStore = create_vector_store(docs)
+def process_chat(chain, question, chat_history):
+    response = chain.invoke({
+        "input": question,
+        "chat_history": chat_history
+    })
 
-chain = create_chain(vectorStore)
+    return response["answer"]
 
-response = chain.invoke({
-    "input": "What is LCEL?",
-})
+if __name__ == '__main__':
+    print("here") # doesn't prompt without this?
+    docs = get_documents_from_web("https://python.langchain.com/docs/expression_language/")
+    vectorStore = create_vector_store(docs)
+    chain = create_chain(vectorStore)
 
-print(response["answer"])
+    chat_history = []
 
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() == "exit":
+            break
+
+        response = process_chat(chain, user_input, chat_history)
+        chat_history.append(HumanMessage(content=user_input))
+        chat_history.append(AIMessage(content=response))
+        print("assistant:", response)
